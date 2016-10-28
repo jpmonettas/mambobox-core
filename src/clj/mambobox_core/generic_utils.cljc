248fcebeb42b571020a61d0e3dfdcb8dbf011f85
Juan Monetta <jpmonettas@gmail.com>
2016-10-28 08:30:48 -0300

    Implemented denormalize-entity-name-string

--- src/clj/mambobox_core/generic_utils.cljc
+++ src/clj/mambobox_core/generic_utils.cljc
@@ -1,5 +1,13 @@
 (ns mambobox-core.generic-utils
-  (:require [camel-snake-kebab.core :as csk]))
+  (:require [camel-snake-kebab.core :as csk]
+            [clojure.string :as str]))
 
 (defn normalize-entity-name-string [name-str]
   (csk/->kebab-case name-str))
+
+(defn denormalize-entity-name-string [norm-name-str]
+  (as-> norm-name-str $
+      (str/split $ #"-") 
+      (update $ 0 str/capitalize)
+      (str/join " " $)))
+
--- test/mambobox_core/generic_utils_test.clj
+++ test/mambobox_core/generic_utils_test.clj
@@ -7,3 +7,9 @@
          (normalize-entity-name-string "ElGranComboDePuertoRico")
          (normalize-entity-name-string "El GranCombo DePuertoRico")
          (normalize-entity-name-string "El-GranCombo_DePuertoRico"))))
+
+(deftest denormalize-entity-name-string-test
+  (is (= "El gran combo de puerto rico"
+         (denormalize-entity-name-string (normalize-entity-name-string "ElGranComboDePuertoRico"))
+         (denormalize-entity-name-string (normalize-entity-name-string "El GranCombo DePuertoRico"))
+         (denormalize-entity-name-string (normalize-entity-name-string "El-GranCombo_DePuertoRico")))))
